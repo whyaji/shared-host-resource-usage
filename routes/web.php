8e3875bb05f958c1e296932d17dec6af10994330
Wahyu Patriaji <wahyupatriaji@gmail.com>
2025-10-17 12:31:23 +0700

    refactor: remove default route response for the API

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'Laravel API is running',
-        'version' => app()->version(),
-        'timestamp' => now()
-    ]);
-});
